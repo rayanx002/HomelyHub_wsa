2458d1880000bed8f2fffe11120237a1e91a8f3a
Rayan <rahularyanraj8@gmail.com>
2026-09-20 19:35:15 +0530

    created the .env file in Frontend and done some changes in index.js of Backend

--- HomelyHub_WebApp/backend/src/index.js
+++ HomelyHub_WebApp/backend/src/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import {router} from "./routes/userRoutes.js";
+import { router } from "./routes/userRoutes.js";
 import { propertyRouter } from "./routes/propertyRouter.js";
 import { bookingRouter } from "./routes/bookingRouter.js";
 import { tripRouter } from "./routes/tripRouter.js";
@@ -15,16 +15,18 @@ const app = express();
 
 //express.json
 
-app.use(express.json({limit: "100mb"}));
+app.use(express.json({ limit: "100mb" }));
 
 //urlencoded
-app.use(express.urlencoded({limit: "100mb", extended: true}));
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
 
 //cookie-parser
 app.use(cookieParser());
 
-
-
+app.use(cors({
+  origin: process.env.ORIGIN_ACCESS_URL,
+  credentials: true
+}))
 
 
 const PORT = process.env.PORT;
@@ -32,7 +34,7 @@ const PORT = process.env.PORT;
 // Test Route
 app.get("/", (req, res) => {
   res.send("HomelyHub Backend is running successfully!");
-}); 
+});
 
 app.use("/api/v1/rent/user", router);
 app.use("/api/v1/rent/listing", propertyRouter);
