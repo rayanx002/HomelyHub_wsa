7658b938f50259b600845e006a1f0a8f82ea4b89
Rayan <rahularyanraj8@gmail.com>
2026-09-23 23:12:46 +0530

    Updated the backend url in vite.config.js

--- HomelyHub_WebApp/Frontend/vite.config.js
+++ HomelyHub_WebApp/Frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy:{
       '/api':{
-        target: 'http://localhost:8080',
+        target: "https://homelyhub-rayanx.onrender.com",
         changeOrigin: true,
         secure: false,
       }
